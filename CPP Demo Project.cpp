4a1e7b18fcf4e73bab7a1b3a24d25715909a4927
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-03-18 22:23:40 +0000

    initial commit
    sets up solution

---
+++ CPP Demo Project.cpp
@@ -0,0 +1,12 @@
+﻿// CPP Demo Project.cpp : Defines the entry point for the application.
+//
+
+#include "CPP Demo Project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
