fc84186f10eaa21fcf36efa3fe4a4a7045ca3c34
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-03-18 22:54:35 +0000

    adds template main/demo calls

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿// CPP Demo Project.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+#include "src/classes/player.h"
+#include "src/classes/tic_tac_toe.h"
+
+using namespace std;
+
+int main()
+{
+
+    TicTacToe game;
+    Player currentPlayer = Player::X;
+
+    int row = 0;
+    int col = 0;
+    cout << "---\nHello world!\n---";
+
+    // Demo calls
+    game.displayBoard();
+    game.makeMove(row, col, currentPlayer);
+}
