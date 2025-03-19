77a55a5ff766dc9af42974d3481229c5ec0a74b1
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-03-19 23:35:00 +0000

    adds input loop to main

--- main.cpp
+++ main.cpp
@@ -13,11 +13,49 @@ int main()
 
     int row = 0;
     int col = 0;
-    cout << "---\nHello world!\n---\n";
+    
+    bool game_active = true;
+    bool input_valid = false;
+
+    while (game_active) {
+        row = 0;
+        col = 0;
+
+        while (!input_valid) {
+            // cmd only
+            system("cls");
+            game.displayBoard();
+            cout << "Player " << ((currentPlayer == Player::X) ? 'X' : 'O') << ", enter row (between 1 & 3): ";
+            cin >> row;
+            cout << "Player " << ((currentPlayer == Player::X) ? 'X' : 'O') << ", enter column (between 1 & 3): ";
+            cin >> col;
+            try {
+                if ((row >= 1) && (row <= 3) && (col >= 1) && (col <= 3)) {
+                    game.makeMove(row, col, currentPlayer);
+                    cout << "\nMade move!\n";
+                    input_valid = true;
+                }
+                else {
+                    //TODO add row/col to error
+                    string error_message = "Invalid input!";
+                    throw(error_message);
+                }
+            }
+            catch (string error_string) {
+                cout << error_string;
+                input_valid = true;
+            }
+        };
+        
+
+
+
+
+    };
 
     // Demo calls
-    game.displayBoard();
-    game.makeMove(row, col, currentPlayer);
-    game.checkWin(currentPlayer);
+    //game.displayBoard();
+    //game.makeMove(row, col, currentPlayer);
+    //game.checkWin(currentPlayer);
 
 }
