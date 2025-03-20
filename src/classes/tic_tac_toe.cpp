7138a66e793a39e231f78b75260cdba1508815e5
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-03-20 00:31:48 +0000

    adds win checking condition

--- src/classes/tic_tac_toe.cpp
+++ src/classes/tic_tac_toe.cpp
@@ -51,6 +51,23 @@ bool TicTacToe::makeMove(int row, int col, Player player) {
 }
 
 bool TicTacToe::checkWin(Player player) {
-    std::cout << "Checking for a win...\n";
+    // get symbol according to enum
+    char symbol = (player == Player::X) ? 'X' : 'O';
+
+    // Check rows and columns - if any row or column is filled iwth this players symbol, they win
+    for (int i = 0; i < 3; ++i) {
+        if ((board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) ||
+            (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if ((board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
+        (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)) {
+        return true;
+    }
+    // player has not won yet
     return false;
 }
+
